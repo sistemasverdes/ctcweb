67899e9cd38f326c1a1ad5545d493e0e3d6e760c
Julian Diaz <julian.sistema@gmail.com>
2022-01-22 08:26:14 -0300

    agregada sección Documentacion con sus descargas

--- resources/views/documentacion.blade.php
+++ resources/views/documentacion.blade.php
@@ -83,23 +83,23 @@ https://templatemo.com/tm-567-nomad-force
                 <div class="col-lg-9 col-12 mt-3 mb-lg-5">
                     <p class="me-4" data-aos="fade-up" data-aos-delay="300">
                     <ul>
-                        <li><a href="">Declaración de Interés nª 59 de Cámara de Diputados</a></li>
+                        <li><a href="{{ asset('descargas/declaracion_diputados.pdf') }}">Declaración de Interés nª 59 de Cámara de Diputados</a></li>
 
-                        <li><a href="">Constancia de Adhesión INTI.</a></li>
+                        <li><a href="{{ asset('descargas/Constancia_Adhesión_INTI.pdf') }}">Constancia de Adhesión INTI.</a></li>
 
-                        <li><a href="">Constancia RIN.</a></li>
+                        <li><a href="{{ asset('descargas/Constancia_RIN.pdf') }}">Constancia RIN.</a></li>
 
-                        <li><a href="">Matricula INAES.</a></li>
+                        <li><a href="{{ asset('descargas/Matricula_INAES.pdf') }}">Matricula INAES.</a></li>
 
-                        <li><a href="">Habilitación de Local.</a></li>
+                        <li><a href="{{ asset('descargas/HABILITACION.pdf') }}">Habilitación de Local.</a></li>
 
-                        <li><a href="">Inscripción en Registro de Proveedores de la Municipalidad de Córdoba.</a></li>
+                        <li><a href="{{ asset('descargas/INSCRIPCION_EN_REGISTRO_DE_PROVEEDORES_MUNICIPALIDAD_DE_CORDOBA.pdf') }}">Inscripción en Registro de Proveedores de la Municipalidad de Córdoba.</a></li>
 
-                        <li><a href="">Inscripción en Registro de Proveedores de la Provincia de Entre Ríos.</a></li>
+                        <li><a href="{{ asset('descargas/INSCRIPCION_EN_REGISTRO_DE_PROVEEDORES_PROVINCIA_DE_ENTRE_RIOS.pdf') }}">Inscripción en Registro de Proveedores de la Provincia de Entre Ríos.</a></li>
 
-                        <li><a href="">Inscripción en Regristro de Proveedores de la Provincia de Santa Fe.</a></li>
+                        <li><a href="{{ asset('descargas/INSCRIPCION_EN_REGISTRO_DE_PROVEEDORES_PROVINCIA_SANTA_FE.pdf') }}">Inscripción en Regristro de Proveedores de la Provincia de Santa Fe.</a></li>
                         <ul>
-                            </p>
+                    </p>
 
                 </div>
             </div>
